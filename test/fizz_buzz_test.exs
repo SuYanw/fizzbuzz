74f0614704c073d4251c1d0c689774a3cc509021
glaubert <suyann12@hotmail.com>
2022-06-07 21:07:28 -0300

    Update unittest

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "buid/'" do
+    test "When a valid file is provided, return the converted list" do
+      expect_response = {:ok, [1, 2, :fizz, 4, 8, :fizzbuzz, :fizz]}
+
+      assert FizzBuzz.build("numeros.txt") == expect_response
+    end
+    
+    test "When an invalid file is provided, returns an error" do
+      expect_response = {:error, "Error reading the file: enoent"}
+      
+      assert FizzBuzz.build("invalid.txt") == expect_response
+    end
   end
 end
